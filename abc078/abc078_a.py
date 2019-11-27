179e54ff6420a2042ce98aebbffac189612ff35d
rano <0x1feb@gmail.com>
2019-11-27 21:25:55 +0900

    Finish A, B in AtCoder Beginner Contest 078

---
+++ abc078/abc078_a.py
@@ -0,0 +1,10 @@
+X, Y = input().split()
+
+if X < Y:
+    ans = "<"
+elif X > Y:
+    ans = ">"
+else:
+    ans = "="
+
+print(ans)
---
+++ abc078/abc078_b.py
@@ -0,0 +1,4 @@
+X, Y, Z = map(int, input().split())
+ans = (X - Z) // (Y + Z)
+
+print(ans)
