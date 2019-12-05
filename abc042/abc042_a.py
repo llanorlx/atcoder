72df960865e806c2a42ed2633a067f2a5687211f
rano <0x1feb@gmail.com>
2019-12-05 22:17:49 +0900

    Finish A, B in AtCoder Beginner Contest 042

---
+++ abc042/abc042_a.py
@@ -0,0 +1,6 @@
+abc = input()
+
+if abc.count("5") == 2 and abc.count("7") == 1:
+    print("YES")
+else:
+    print("NO")
---
+++ abc042/abc042_b.py
@@ -0,0 +1,4 @@
+N, L = map(int, input().split())
+S = [input() for _ in range(N)]
+
+print("".join(sorted(S)))
