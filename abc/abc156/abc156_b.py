ce8810fdbb2fd652a49a9d4ba33d99e4db1c0abd
rano <0x1feb@gmail.com>
2020-02-23 00:22:45 +0900

    Finish A, B, C in AtCoder Beginner Contest 156

---
+++ abc/abc156/abc156_b.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+
+ans = 0
+
+while True:
+    N //= K
+    ans += 1
+    if N == 0:
+        break
+
+print(ans)
---
+++ abc/abc156/abc156_c.py
@@ -0,0 +1,12 @@
+N = int(input())
+X = list(map(int, input().split()))
+
+ans = 10 ** 9
+
+for i in range(1, 101):
+    total = 0
+    for x in X:
+        total += (x - i) ** 2
+    ans = min(total, ans)
+
+print(ans)
