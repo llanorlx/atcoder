9f61503567c16f772a8b52debf4ad51561f58955
rano <0x1feb@gmail.com>
2020-12-25 09:09:03 +0900

    Finish A, B in AtCoder Beginner Contest 184

---
+++ abc/abc184/abc184_b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+const int MAX_T = 200005;
+
+int main() {
+  int N, X;
+  cin >> N >> X;
+  string S;
+  cin >> S;
+  int score = X;
+  for (char s : S) {
+    score += (s == 'o' ? 1 : -1);
+    maxs(score, 0);
+  }
+
+  cout << score << endl;
+  return 0;
+}
