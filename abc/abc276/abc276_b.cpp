d7739c1d58bcb2f5ef04f07b22e2b95e9edda7ff
rano <0x1feb@gmail.com>
2022-11-06 23:35:40 +0900

    Finish A, B, C, D in AtCoder Beginner Contest 276

---
+++ abc/abc276/abc276_b.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vvi x(n + 1);
+  rep(i, m) {
+    int a, b;
+    cin >> a >> b;
+    x[a].push_back(b);
+    x[b].push_back(a);
+  }
+  rrep(i, n) {
+    sort(rng(x[i]));
+    cout << x[i].size();
+    for (int xi : x[i]) {
+      cout << " " << xi;
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
