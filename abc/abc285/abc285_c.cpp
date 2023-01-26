c8aced9673ed82953b38b5349b6346986a5650a9
rano <0x1feb@gmail.com>
2023-01-26 17:31:20 +0900

    Finish A, B, C in AtCoder Beginner Contest 285

---
+++ abc/abc285/abc285_c.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  string s;
+  cin >> s;
+
+  map<char, int> c;
+  int num = 1;
+  for (char x = 'A'; x <= 'Z'; x++) c[x] = num++;
+  int s_size = s.size();
+  ll ans = 0, t = 1;
+  drep(i, s_size) {
+    ans += c[s[i]] * t;
+    t *= 26;
+  }
+  cout << ans << endl;
+  return 0;
+}
