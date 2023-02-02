130038d684de5438e7fca9e8f991eeec69bbf052
rano <0x1feb@gmail.com>
2023-02-02 20:03:27 +0900

    Finish A, B in AtCoder Beginner Contest 270

---
+++ abc/abc270/abc270_b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int x, y, z;
+  cin >> x >> y >> z;
+
+  if (y < 0) {
+    x *= -1;
+    y *= -1;
+    z *= -1;
+  }
+  if (x < y) {
+    cout << abs(x) << endl;
+  } else {
+    if (y < z) {
+      cout << -1 << endl;
+    } else {
+      cout << abs(z) + abs(x - z) << endl;
+    }
+  }
+  return 0;
+}
