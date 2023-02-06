6195f9aff75f3bb0c5c7222efccc73baaa874630
rano <0x1feb@gmail.com>
2023-02-06 22:55:57 +0900

    Finish A, B in AtCoder Beginner Contest 287

---
+++ abc/abc287/abc287_b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+  vector<string> t(m);
+  rep(i, m) cin >> t[i];
+
+  int ans = 0;
+  for (string si : s) {
+    for (string ti : t) {
+      if (si.substr(3, 3) == ti) {
+        ans++;
+        break;
+      }
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
