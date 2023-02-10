0b3f4ca3f8ed88600fa46ef4dc10dfe075517712
rano <0x1feb@gmail.com>
2023-02-10 19:15:09 +0900

    Finish C in AtCoder Beginner Contest 288

---
+++ abc/abc288/abc288_c.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vvi g(n);
+  rep(i, m) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+
+  vector<bool> visited(n, false);
+  queue<int> que;
+  int c = 0;
+  rep(i, n) {
+    if (visited[i]) continue;
+    c++;
+    que.push(i);
+    while (!que.empty()) {
+      int u = que.front();
+      que.pop();
+      visited[u] = true;
+      for (int v : g[u]) {
+        if (visited[v]) continue;
+        que.push(v);
+      }
+    }
+  }
+  int ans = m - n + c;
+  cout << ans << endl;
+  return 0;
+}
