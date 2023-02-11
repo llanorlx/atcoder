f11bbd97b27d59bfa6dad84c3e46f263b78aecb6
rano <0x1feb@gmail.com>
2023-02-11 19:56:09 +0900

    Finish A, B in AtCoder Beginner Contest 269

---
+++ abc/abc269/abc269_b.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n = 10;
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+
+  int a = n + 1, b = 0, c = n + 1, d = 0;
+  rep(i, n) rep(j, n) {
+    if (s[i][j] == '.') continue;
+    mins(a, i + 1);
+    maxs(b, i + 1);
+    mins(c, j + 1);
+    maxs(d, j + 1);
+  }
+  cout << a << " " << b << endl;
+  cout << c << " " << d << endl;
+  return 0;
+}
