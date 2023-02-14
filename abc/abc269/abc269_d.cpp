bb34eb40d9681e66b895471bda1fc5b2ee5c3a38
rano <0x1feb@gmail.com>
2023-02-14 21:20:03 +0900

    Finish D in AtCoder Beginner Contest 269

---
+++ abc/abc269/abc269_d.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<P> p(n);
+  rep(i, n) cin >> p[i].first >> p[i].second;
+
+  map<P, int> memo;
+  rep(i, n) memo[p[i]] = i;
+  vector<P> d = {P(0, 1), P(1, 0), P(1, 1)};
+  dsu uf(n);
+  rep(i, n) for (auto [dx, dy] : d) {
+    P np = make_pair(p[i].first + dx, p[i].second + dy);
+    if (memo.count(np)) uf.merge(i, memo[np]);
+  }
+  int ans = 0;
+  rep(i, n) if (uf.leader(i) == i) ans++;
+  cout << ans << endl;
+  return 0;
+}
