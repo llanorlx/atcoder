ec86b12aacb712015d3e66f24a42f934c383fb64
rano <0x1feb@gmail.com>
2023-03-02 00:16:17 +0900

    Finish C in AtCoder Beginner Contest 291

---
+++ abc/abc291/abc291_c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+
+  set<P> points;
+  int x = 0, y = 0;
+  points.emplace(x, y);
+  for (char si : s) {
+    if (si == 'R') x++;
+    if (si == 'L') x--;
+    if (si == 'U') y++;
+    if (si == 'D') y--;
+    points.emplace(x, y);
+  }
+  cout << (points.size() == n + 1 ? "No" : "Yes") << endl;
+  return 0;
+}
