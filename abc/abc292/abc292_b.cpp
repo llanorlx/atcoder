4b3e0e3b7f339e532dd9ae78f0454259697cc45e
rano <0x1feb@gmail.com>
2023-03-05 20:32:02 +0900

    Finish A, B in AtCoder Beginner Contest 292

---
+++ abc/abc292/abc292_b.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  vi info(n + 1);
+  rep(i, q) {
+    int c, x;
+    cin >> c >> x;
+    if (c == 1) info[x] += 1;
+    if (c == 2) info[x] += 2;
+    if (c == 3) cout << (info[x] >= 2 ? "Yes" : "No") << endl;
+  }
+  return 0;
+}
