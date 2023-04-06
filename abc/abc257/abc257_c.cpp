ce1e4f68bbf304111a18f138a9bfeeecd3eb6507
rano <0x1feb@gmail.com>
2023-04-06 09:15:25 +0900

    Finish C in AtCoder Beginner Contest 257

---
+++ abc/abc257/abc257_c.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  vector<P> memo(n);
+  int ans = 0;
+  rep(i, n) {
+    int w;
+    cin >> w;
+    memo[i] = make_pair(i, w);
+    if (s[i] == '1') ans++;
+  }
+
+  sort(rng(memo), [](P a, P b) -> bool { return a.second < b.second; });
+  int x = ans;
+  rep(i, n) {
+    int num = memo[i].first;
+    x += (s[num] == '0' ? 1 : -1);
+    if (i < n - 1 && memo[i].second == memo[i + 1].second) continue;
+    maxs(ans, x);
+  }
+
+  cout << ans << endl;
+  return 0;
+}
