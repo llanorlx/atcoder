29b4b1f47f40900c181c1fbc2da219f2ab6a61e9
rano <0x1feb@gmail.com>
2023-04-13 22:47:49 +0900

    Finish A, B in AtCoder Beginner Contest 252

---
+++ abc/abc252/abc252_b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vi a(n), b(k);
+  rep(i, n) cin >> a[i];
+  rep(i, k) cin >> b[i];
+
+  int a_max = 0;
+  for (int ai : a) maxs(a_max, ai);
+  for (int bi : b) {
+    if (a[bi - 1] == a_max) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
