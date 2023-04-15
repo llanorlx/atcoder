1309c569d3ce43cfbe5c773506747d6ebc35f06d
rano <0x1feb@gmail.com>
2023-04-16 02:56:24 +0900

    Finish A, B in AtCoder Beginner Contest 250

---
+++ abc/abc250/abc250_a.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  int r, c;
+  cin >> r >> c;
+
+  r--;
+  c--;
+  int ans = 0;
+  rep(x, h) rep(y, w) {
+    if (x == r && y == c) continue;
+    if (abs(x - r) + abs(y - c) == 1) ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
