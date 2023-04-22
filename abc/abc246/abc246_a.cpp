bec6c324d85e3a0bee8906371f76476786e78795
rano <0x1feb@gmail.com>
2023-04-22 23:47:52 +0900

    Finish A, B in AtCoder Beginner Contest 246

---
+++ abc/abc246/abc246_a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  vi x(3), y(3);
+  rep(i, 3) cin >> x[i] >> y[i];
+
+  int x4 = x[0] ^ x[1] ^ x[2], y4 = y[0] ^ y[1] ^ y[2];
+  cout << x4 << " " << y4 << endl;
+  return 0;
+}
