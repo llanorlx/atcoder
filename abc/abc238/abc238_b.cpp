5c6e0398b44c2345d4ad63979a04454aff1a4814
rano <0x1feb@gmail.com>
2023-05-05 18:24:41 +0900

    Finish A, B, C in AtCoder Beginner Contest 238

---
+++ abc/abc238/abc238_b.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vi a(n);
+  rep(i, n) cin >> a[i];
+
+  vi b;
+  b.push_back(0);
+  b.push_back(360);
+  int r = 0;
+  rep(i, n) {
+    b.push_back(360 - (a[i] + r) % 360);
+    r += a[i];
+  }
+  sort(rng(b));
+  int ans = 0;
+  rep(i, b.size()) maxs(ans, b[i + 1] - b[i]);
+  cout << ans << endl;
+  return 0;
+}
