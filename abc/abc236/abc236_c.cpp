fba622fe4e772f00799bd29be3018f7f9bddc86a
rano <0x1feb@gmail.com>
2023-05-07 11:37:30 +0900

    Finish A, B, C in AtCoder Beginner Contest 236

---
+++ abc/abc236/abc236_c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vec<string> s(n);
+  rep(i, n) cin >> s[i];
+  unordered_map<string, bool> exp_stations;
+  rep(i, m) {
+    string t;
+    cin >> t;
+    exp_stations[t] = true;
+  }
+
+  for (string si : s) {
+    cout << (exp_stations[si] ? "Yes" : "No") << endl;
+  }
+  return 0;
+}
