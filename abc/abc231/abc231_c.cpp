92ccade5fb4f1106f4ded0a9478ea125e1ccc6fa
rano <0x1feb@gmail.com>
2023-05-12 10:52:10 +0900

    Finish A, B, C in AtCoder Beginner Contest 231

---
+++ abc/abc231/abc231_c.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  vi a(n);
+  rep(i, n) cin >> a[i];
+
+  sort(rng(a));
+  auto f = [&](int x) -> int {
+    int ng = -1, ok = n;
+    while (abs(ok - ng) > 1) {
+      int mid = (ok + ng) / 2;
+      if (a[mid] >= x) {
+        ok = mid;
+      } else {
+        ng = mid;
+      }
+    }
+    return n - ok;
+  };
+
+  rep(i, q) {
+    int xi;
+    cin >> xi;
+    cout << f(xi) << endl;
+  }
+  return 0;
+}
