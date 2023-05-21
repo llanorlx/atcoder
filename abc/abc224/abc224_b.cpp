730e76b737c319b429a829868fe122002bf19d78
rano <0x1feb@gmail.com>
2023-05-21 20:45:45 +0900

    Finish A, B in AtCoder Beginner Contest 224

---
+++ abc/abc224/abc224_b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  vvi a(h, vi(w));
+  rep(i, h) rep(j, w) cin >> a[i][j];
+
+  rep(i1, h - 1) srep(i2, i1 + 1, h) rep(j1, w - 1) srep(j2, j1 + 1, w) {
+    if (a[i1][j1] + a[i2][j2] > a[i2][j1] + a[i1][j2]) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  cout << "Yes" << endl;
+  return 0;
+}
