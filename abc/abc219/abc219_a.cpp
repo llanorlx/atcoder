927b20181daca79df00d924ef1ba6a10d0efe634
rano <0x1feb@gmail.com>
2023-05-30 20:49:43 +0900

    Finish A, B in AtCoder Beginner Contest 219

---
+++ abc/abc219/abc219_a.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int x;
+  cin >> x;
+
+  auto f = [](int p) -> string {
+    if (p < 40) return to_string(40 - p);
+    if (p < 70) return to_string(70 - p);
+    if (p < 90) return to_string(90 - p);
+    return "expert";
+  };
+
+  cout << f(x) << endl;
+  return 0;
+}
