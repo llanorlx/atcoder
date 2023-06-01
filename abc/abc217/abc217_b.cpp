9aa08842fd15d4145666f49720580debc4e4c7c5
rano <0x1feb@gmail.com>
2023-06-01 22:33:49 +0900

    Finish A, B in AtCoder Beginner Contest 217

---
+++ abc/abc217/abc217_b.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  set<string> st;
+  rep(i, 3) {
+    string s;
+    cin >> s;
+    st.insert(s);
+  }
+
+  for (string contest : {"ABC", "ARC", "AGC", "AHC"}) {
+    if (st.count(contest) == 0) {
+      cout << contest << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
