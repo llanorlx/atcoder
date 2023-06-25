13e8880cfa11a4ea3729dbcfd50546c6a84e0711
rano <0x1feb@gmail.com>
2023-06-25 19:16:02 +0900

    Finish A, B in AtCoder Beginner Contest 199

---
+++ abc/abc199/abc199_b.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  int a_max = 0;
+  rep(i, n) {
+    int a;
+    cin >> a;
+    maxs(a_max, a);
+  }
+  int b_min = INF;
+  rep(i, n) {
+    int b;
+    cin >> b;
+    mins(b_min, b);
+  }
+  int ans = max(0, b_min - a_max + 1);
+  cout << ans << endl;
+  return 0;
+}
