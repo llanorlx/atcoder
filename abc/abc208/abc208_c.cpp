6d3164e092a657bc1f29301186f2e7e13dac5585
rano <0x1feb@gmail.com>
2023-07-09 11:04:49 +0900

    Finish C in AtCoder Beginner Contest 208

---
+++ abc/abc208/abc208_c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  ll n, k;
+  cin >> n >> k;
+  vec<P> citizens(n);
+  rep(i, n) {
+    int a;
+    cin >> a;
+    citizens[i] = make_pair(a, i);
+  }
+
+  sort(rng(citizens));
+  vec<ll> cnt(n, k / n);
+  rep(i, k % n) cnt[citizens[i].second]++;
+  rep(i, n) cout << cnt[i] << endl;
+  return 0;
+}
