93200dd022e9216d168a791a81a8198a46f47589
rano <0x1feb@gmail.com>
2023-07-31 19:21:06 +0900

    Finish B in AtCoder Beginner Contest 310

---
+++ abc/abc310/abc310_b.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vi p(n), c(n);
+  vec<set<int>> f(n);
+  rep(i, n) {
+    cin >> p[i] >> c[i];
+    rep(j, c[i]) {
+      int x;
+      cin >> x;
+      f[i].insert(x);
+    }
+  }
+
+  auto check = [&]() -> bool {
+    rep(i, n) rep(j, n) {
+      if (i == j) continue;
+      bool cond = p[i] >= p[j];
+      cond &= includes(rng(f[j]), rng(f[i]));
+      cond &= p[i] > p[j] || f[j].size() > f[i].size();
+      if (cond) return true;
+    }
+    return false;
+  };
+
+  cout << (check() ? "Yes" : "No") << endl;
+  return 0;
+}
