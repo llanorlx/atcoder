507bc6bfbc725e0895e4c841d1eb4454272df64f
rano <0x1feb@gmail.com>
2023-08-10 22:36:51 +0900

    Finish B in AtCoder Beginner Contest 313

---
+++ abc/abc313/abc313_b.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vi rank(n);
+  rep(i, m) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    rank[b]++;
+  }
+
+  int ans = -1;
+  rep(i, n) {
+    if (rank[i] > 0) continue;
+    if (ans == -1) {
+      ans = i + 1;
+    } else {
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
