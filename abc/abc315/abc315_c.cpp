11427344999bcf0d60f3503010a6c5bfe0229716
rano <0x1feb@gmail.com>
2023-08-30 15:57:37 +0900

    Finish C in AtCoder Beginner Contest 315

---
+++ abc/abc315/abc315_c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vec<P> cups(n);
+  rep(i, n) cin >> cups[i].second >> cups[i].first;
+
+  sort(rng(cups));
+  P s = cups[n - 1];
+  int ans = 0;
+  rep(i, n - 1) {
+    P t = cups[i];
+    int x = s.second == t.second ? s.first + t.first / 2 : s.first + t.first;
+    maxs(ans, x);
+  }
+  cout << ans << endl;
+  return 0;
+}
