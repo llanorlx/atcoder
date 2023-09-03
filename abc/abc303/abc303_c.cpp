e0ac01a21df0628d3ea5338eac6932b61cf8a913
rano <0x1feb@gmail.com>
2023-09-03 17:46:36 +0900

    Finish C in AtCoder Beginner Contest 303

---
+++ abc/abc303/abc303_c.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m, h, k;
+  cin >> n >> m >> h >> k;
+  string s;
+  cin >> s;
+  map<int, map<int, bool>> is_items;
+  rep(i, m) {
+    int x, y;
+    cin >> x >> y;
+    is_items[x][y] = true;
+  }
+
+  int px = 0, py = 0;
+  auto move = [&](char c) -> void {
+    if (c == 'R') px++;
+    if (c == 'L') px--;
+    if (c == 'U') py++;
+    if (c == 'D') py--;
+    h--;
+  };
+
+  for (char si : s) {
+    move(si);
+    if (h < 0) {
+      cout << "No" << endl;
+      return 0;
+    }
+    if (is_items[px][py] && h < k) {
+      is_items[px][py] = false;
+      h = k;
+    }
+  }
+  cout << "Yes" << endl;
+  return 0;
+}
