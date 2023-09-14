0fcaeb28e8efb5905fa5b1158bdf0cf60530d09d
rano <0x1feb@gmail.com>
2023-09-14 23:17:26 +0900

    Finish A in AtCoder Beginner Contest 319

---
+++ abc/abc319/abc319_a.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  map<string, int> players = {
+      {"tourist", 3858},   {"ksun48", 3679},  {"Benq", 3658},
+      {"Um_nik", 3648},    {"apiad", 3638},   {"Stonefeang", 3630},
+      {"ecnerwala", 3613}, {"mnbvmar", 3555}, {"newbiedmy", 3516},
+      {"semiexp", 3481},
+  };
+  string s;
+  cin >> s;
+  cout << players[s] << endl;
+  return 0;
+}
