33c434863693ccbb5395bf535faa9bea3e276399
rano <0x1feb@gmail.com>
2023-09-27 10:08:12 +0900

    Finish C in AtCoder Beginner Contest 233

---
+++ abc/abc233/abc233_c.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  ll x;
+  cin >> n >> x;
+  vvec<ll> a(n);
+  rep(i, n) {
+    int l;
+    cin >> l;
+    a[i].resize(l);
+    rep(j, l) cin >> a[i][j];
+  }
+
+  int ans = 0;
+  auto dfs = [&](auto dfs, int i, ll t) -> void {
+    if (t > x) return;
+    if (i == n) {
+      if (x == t) ans++;
+      return;
+    }
+    for (ll v : a[i]) {
+      if (v > x / t) continue;
+      dfs(dfs, i + 1, t * v);
+    }
+  };
+  dfs(dfs, 0, 1);
+  cout << ans << endl;
+  return 0;
+}
