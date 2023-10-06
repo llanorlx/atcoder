713057eb35412064e45e9a9c1e8db525388fc7ff
rano <0x1feb@gmail.com>
2023-10-06 09:19:33 +0900

    Finish B in AtCoder Beginner Contest 322

---
+++ abc/abc322/abc322_b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  string s, t;
+  cin >> s >> t;
+
+  auto f = [&]() -> int {
+    bool is_suffix = (t.substr(0, n) == s);
+    bool is_preffix = (t.substr(m - n, n) == s);
+    if (is_suffix && is_preffix) return 0;
+    if (is_suffix) return 1;
+    if (is_preffix) return 2;
+    return 3;
+  };
+  cout << f() << endl;
+  return 0;
+}
