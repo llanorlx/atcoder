0767678f81a27466e1bdf58523e9d161b1fc00b7
rano <0x1feb@gmail.com>
2023-11-02 08:58:48 +0900

    Finish C in AtCoder Beginner Contest 268

---
+++ abc/abc268/abc268_c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vi p(n);
+  rep(i, n) cin >> p[i];
+
+  vi cnt(n);
+  rep(i, n) {
+    int j = (p[i] - i - 1 + n) % n;
+    rep(k, 3) cnt[(j + k) % n]++;
+  }
+  int ans = 0;
+  for (int c : cnt) maxs(ans, c);
+  cout << ans << endl;
+  return 0;
+}
