9d953b81e7f251293a488383278323ca472e3c64
rano <0x1feb@gmail.com>
2023-11-03 15:26:04 +0900

    Finish C in AtCoder Beginner Contest 321

---
+++ abc/abc321/abc321_c.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int k;
+  cin >> k;
+
+  vec<ll> a;
+  srep(i, 2, 1 << 10) {
+    ll x = 0;
+    drep(j, 10) {
+      if (i & (1 << j)) {
+        x *= 10;
+        x += j;
+      }
+    }
+    a.push_back(x);
+  }
+
+  sort(rng(a));
+  cout << a[k - 1] << endl;
+  return 0;
+}
