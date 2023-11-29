7e0ad854a256f88e4d19f9b03ff80687c4cb63fa
rano <0x1feb@gmail.com>
2023-11-29 20:05:40 +0900

    Finish C in AtCoder Beginner Contest 330

---
+++ abc/abc330/abc330_c.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  ll d;
+  cin >> d;
+
+  ll ans = d;
+  for (ll x = 0; x <= sqrt(d) + 1; x++) {
+    ll a = x * x - d;
+    if (a > 0) {
+      mins(ans, abs(x * x - d));
+    } else {
+      ll y = sqrt(abs(a));
+      rep(i, 2) mins(ans, abs(x * x + (y + i) * (y + i) - d));
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
