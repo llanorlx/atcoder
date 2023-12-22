15dfc4a5a3bb1277cbc35844ae0ec1dab0bea276
rano <0x1feb@gmail.com>
2023-12-22 09:36:36 +0900

    Finish B in AtCoder Beginner Contest 333

---
+++ abc/abc333/abc333_b.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  auto is_short = [](char x, char y) -> bool {
+    int d = abs(x - y);
+    return d == 1 || d == 4;
+  };
+
+  bool is_eq = is_short(s[0], s[1]) == is_short(t[0], t[1]);
+  cout << (is_eq ? "Yes" : "No") << endl;
+  return 0;
+}
