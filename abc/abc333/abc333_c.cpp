e07866d446587ea72e22fadd0ca84af9ebb7ad89
rano <0x1feb@gmail.com>
2023-12-24 23:59:46 +0900

    Finish C in AtCoder Beginner Contest 333

---
+++ abc/abc333/abc333_c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int m = 12;
+  vec<ll> x(m);
+  x[0] = 1;
+  rep(i, m - 1) x[i + 1] = x[i] * 10 + 1;
+  rep(i, m) rep(j, i + 1) rep(k, j + 1) {
+    n--;
+    if (n == 0) {
+      cout << x[i] + x[j] + x[k] << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
