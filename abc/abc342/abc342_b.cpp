327ff10500b843079b7f901c4d64968178c4766e
rano <0x1feb@gmail.com>
2024-02-29 22:32:07 +0900

    Finish B in AtCoder Beginner Contest 342

---
+++ abc/abc342/abc342_b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vi p(n), s(n + 1);
+  rep(i, n) {
+    cin >> p[i];
+    s[p[i]] = i;
+  }
+  int q;
+  cin >> q;
+  rep(i, q) {
+    int a, b;
+    cin >> a >> b;
+    cout << p[min(s[a], s[b])] << endl;
+  }
+  return 0;
+}
