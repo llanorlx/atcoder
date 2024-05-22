844c6825a6d576fdcd2e5ee38636e98c52cb035d
rano <0x1feb@gmail.com>
2024-05-22 23:09:40 +0900

    Finish C in AtCoder Beginner Contest 349

---
+++ abc/abc349/abc349_c.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n)-1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  s += 'x';
+  transform(rng(t), t.begin(), ::tolower);
+
+  auto is_airport_code = [&]() -> bool {
+    int i = 0;
+    for (char c : s) {
+      if (t[i] == c) i++;
+      if (i >= (int)t.size()) return true;
+    }
+    return false;
+  };
+
+  cout << (is_airport_code() ? "Yes" : "No") << endl;
+  return 0;
+}
