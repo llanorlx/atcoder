cb5743188a867f958a3366960512b3c956324588
rano <0x1feb@gmail.com>
2024-07-13 23:34:34 +0900

    Finish B in AtCoder Beginner Contest 355

---
+++ abc/abc355/abc355_b.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vi a(n), b(m);
+  rep(i, n) cin >> a[i];
+  rep(i, m) cin >> b[i];
+
+  vi c;
+  c.insert(c.end(), rng(a));
+  c.insert(c.end(), rng(b));
+  sort(rng(c));
+
+  set<int> sa(rng(a));
+  rep(i, n + m - 1) {
+    if (sa.contains(c[i]) && sa.contains(c[i + 1])) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
