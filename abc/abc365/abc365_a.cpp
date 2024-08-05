1a6c23de15ad657a7932ab4a5bbd722491cd1442
rano <0x1feb@gmail.com>
2024-08-05 10:46:38 +0900

    Finish A in AtCoder Beginner Contest 365

---
+++ abc/abc365/abc365_a.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int y;
+  cin >> y;
+
+  auto y2d = [](int year) -> int {
+    if (year % 4 != 0) return 365;
+    if (year % 100 != 0) return 366;
+    if (year % 400 != 0) return 365;
+    return 366;
+  };
+
+  int ans = y2d(y);
+  cout << ans << endl;
+  return 0;
+}
