e3d2c00956f64a72b92fdcb381f080d1665084aa
rano <0x1feb@gmail.com>
2024-08-14 21:19:29 +0900

    Finish B in AtCoder Beginner Contest 366

---
+++ abc/abc366/abc366_b.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vec<string> s(n);
+  rep(i, n) cin >> s[i];
+
+  int m = 0;
+  for (string si : s) {
+    maxs(m, (int)si.size());
+  }
+
+  vec<string> t(m, string(n, '*'));
+  rep(i, n) rep(j, (int)s[i].size()) {
+    t[j][n - 1 - i] = s[i][j];
+  }
+
+  rep(i, m) {
+    while (t[i].back() == '*') {
+      t[i].pop_back();
+    }
+  }
+
+  for (string ti : t) {
+    cout << ti << endl;
+  }
+  return 0;
+}
