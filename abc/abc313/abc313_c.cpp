41efd81bb41e86abf0df5dd6c3c448bd808eb326
rano <0x1feb@gmail.com>
2024-08-18 12:14:51 +0900

    Finish C in AtCoder Beginner Contest 313

---
+++ abc/abc313/abc313_c.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vec<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  ll s = accumulate(rng(a), (ll)0);
+  ll t = s / n, d1 = 0, d2 = 0;
+  for (ll v : a) {
+    if (v > t) {
+      d1 += v - (t + 1);
+    } else {
+      d2 += t - v;
+    }
+  }
+  ll ans = max(d1, d2);
+  cout << ans << endl;
+  return 0;
+}
