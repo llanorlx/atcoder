9a22bdd00fb7aacf031bba4b00ecb42c6b10ca62
rano <0x1feb@gmail.com>
2024-08-31 12:09:19 +0900

    Finish D in AtCoder Beginner Contest 334

---
+++ abc/abc334/abc334_d.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  vec<ll> r(n);
+  rep(i, n) cin >> r[i];
+
+  sort(rng(r));
+  vec<ll> s(n + 1);
+  rep(i, n) s[i + 1] = s[i] + r[i];
+  auto f = [&n, &s](ll x) -> ll {
+    ll ok = 0, ng = n + 1;
+    while (abs(ok - ng) > 1) {
+      ll mid = (ok + ng) / 2;
+      if (s[mid] <= x) {
+        ok = mid;
+      } else {
+        ng = mid;
+      }
+    }
+    return ok;
+  };
+
+  rep(i, q) {
+    ll query;
+    cin >> query;
+    cout << f(query) << endl;
+  }
+  return 0;
+}
