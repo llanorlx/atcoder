4d2b874e780b4d53ccb70603af28df8ea6c159a8
rano <0x1feb@gmail.com>
2024-09-28 21:17:54 +0900

    Finish D in AtCoder Beginner Contest 358

---
+++ abc/abc358/abc358_d.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vi a(n);
+  rep(i, n) cin >> a[i];
+  vi b(m);
+  rep(i, m) cin >> b[i];
+
+  sort(rng(a));
+  sort(rng(b));
+  auto f = [&]() -> ll {
+    ll costs = 0;
+    int i = 0;
+    for (int count : a) {
+      if (count < b[i]) continue;
+      costs += count;
+      i++;
+      if (i == m) return costs;
+    }
+    return -1;
+  };
+
+  ll ans = f();
+  cout << ans << endl;
+  return 0;
+}
