2db0739cee349008422c56aedf8955c44f543d6c
rano <0x1feb@gmail.com>
2024-10-10 09:41:55 +0900

    Finish B in AtCoder Beginner Contest 372

---
+++ abc/abc372/abc372_b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int m;
+  cin >> m;
+
+  vi a;
+  rep(i, 11) {
+    rep(j, m % 3) a.push_back(i);
+    m /= 3;
+  }
+
+  int a_size = a.size();
+  cout << a_size << endl;
+  rep(i, a_size) cout << a[i] << (i < a_size - 1 ? ' ' : '\n');
+  return 0;
+}
