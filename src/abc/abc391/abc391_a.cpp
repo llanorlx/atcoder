57d0a1729c4832e7956bc7e3aec1e6c978ec444a
rano <0x1feb@gmail.com>
2025-02-17 22:07:47 +0000

    Finish A in AtCoder Beginner Contest 391

---
+++ src/abc/abc391/abc391_a.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  string d;
+  cin >> d;
+
+  auto f = [](string s) -> string {
+    string ans = "";
+    for (char c : s) {
+      if (c == 'N') ans += 'S';
+      if (c == 'E') ans += 'W';
+      if (c == 'W') ans += 'E';
+      if (c == 'S') ans += 'N';
+    }
+    return ans;
+  };
+
+  cout << f(d) << endl;
+  return 0;
+}
